5c9c95e7622b6376675fafac70c83b904d6ee3cf
ismaeelakram <mathcenter562@gmail.com>
2020-06-30 23:56:16 -0400

    Add custom cors

--- src/index.js
+++ src/index.js
@@ -29,8 +29,13 @@ mongoose
   });
 let connection = mongoose.connection;
 
-app.use(cors);
 app.use(bodyParser.json({ limit: "100gb" }));
+
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("API running.");
